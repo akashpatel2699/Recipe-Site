10ee43368aaf5e508c1fe9b2f66c6a53857079e1
EC2 Default User <ec2-user@ip-172-31-69-21.ec2.internal>
2020-09-20 20:35:39 +0000

    Search capability added to let user search certain food receipe and some css added to enhance the look of twitter quote

--- application.py
+++ application.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request
-from tweepy import OAuthHandler,API, Cursor
 import os
 import random
 from dotenv import load_dotenv
+import base64
+import requests
+from time import strftime, strptime
 
 #Load all environments variables 
 # set as system variables
@@ -12,12 +14,28 @@ app=Flask(__name__)
 
 consumer_key=os.environ['consumer_key']
 consumer_secret=os.environ['consumer_secret']
-access_token=os.environ['access_token']
-access_token_secret=os.environ['access_token_secret']
+spoonacular_api_key=os.environ['spoonacular_api_key']
 
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-auth_api = API(auth)
+# base64 Encoding of two key into one for Twitter API Authentication
+key_secret = '{}:{}'.format(consumer_key, consumer_secret).encode('ascii')
+b64_encoded_key = base64.b64encode(key_secret)
+b64_encoded_key = b64_encoded_key.decode('ascii')
+
+base_url = 'https://api.twitter.com/'
+auth_url = '{}oauth2/token'.format(base_url)
+
+auth_headers = {
+    'Authorization': 'Basic {}'.format(b64_encoded_key),
+    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+}
+
+auth_data = {
+    'grant_type': 'client_credentials'
+}
+# POST request to obtain Bearer Token (acces key)
+auth_resp = requests.post(auth_url, headers=auth_headers, data=auth_data)
+
+access_token = auth_resp.json()['access_token']
 
 food_items = [
     "Pasta",
@@ -28,30 +46,65 @@ food_items = [
     "Sandwich",
     "Dosa"
     ]
-def make_search_request(food_name):
-    random_food = random.randint(0,len(food_items)-1)
-    random_tweet = random.randint(0,ITEMS-1)
-    for status in Cursor(auth_api.search,q=food_name,lang="en").items(ITEMS):
-        if (random_tweet) == random_food:
+
+def twitter_search_request(querry):
+    COUNT = 20
+    random_food_index = random.randint(0,len(food_items)-1)
+    random_tweet_index = random.randint(0,COUNT-1)
+    #Search Query to be send to fetch some data from Twitter API endpoint
+    search_headers = {
+        'Authorization': 'Bearer {}'.format(access_token)    
+    }
+    #Options for search parameters 
+    search_params = {
+        'q': '{}'.format(querry),
+        'result_type': 'recent',
+        'lang': 'en',
+        'count': COUNT
+    }
+    
+    search_url = '{}1.1/search/tweets.json'.format(base_url)
+    
+    search_resp = requests.get(search_url, headers=search_headers, params=search_params)
+    
+    tweet_data = search_resp.json()
+    
+    for status in tweet_data['statuses']:
+        if (random_tweet_index) == random_food_index:
             tweet = status
             break
-        elif random_tweet < random_food:
-            random_tweet += 1
+        elif random_tweet_index < random_food_index:
+            random_tweet_index += 1
         else:
-            random_tweet -= 1
+            random_tweet_index -= 1
+    tweet['created_at'] = strftime("%a, %d %b %H:%M:%S %Y",strptime(tweet['created_at'],"%a %b %d %H:%M:%S +0000 %Y"))
     return tweet
 
-    
+def spooncular_receipe_request(query):
+    base_url="https://api.spoonacular.com/recipes/complexSearch"
+    payload = {
+        'apiKey': spoonacular_api_key,
+        'query': query,
+        'instructionsRequire': True,
+        'number': 1
+    }
+    search_result = requests.get(base_url,params=payload)
+    search_result = search_result.json()
+    print(search_result)
+   
+
 ITEMS = 20
 @app.route("/",methods=["GET","POST"])
 def index():
     if request.method == "POST":
-        food_name=request.form.get("food_item")
-        tweet = make_search_request(food_name)
+        food_name=request.form.get("food_name")
+        print(food_name)
+        tweet = twitter_search_request(food_name)
+        spooncular_receipe_request(food_name)
         return render_template("index.html",food_name=food_name,tweet=tweet)
     else:
         food_name = food_items[random.randint(0,len(food_items)-1)]
-        tweet = make_search_request(food_name)
+        tweet = twitter_search_request(food_name)
         return render_template("index.html",food_name=food_name,tweet=tweet)
     
 if __name__ == "__main__":
